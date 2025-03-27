647ed2ca3f453f7e290eb80e9f903dee8a6ce8c1
Ogabek Hamzaqulov <hamzaqulovogabek@gmail.com>
2025-03-27 09:49:31 +0500

    feat: program cs

--- ExcelToWord/Program.cs
+++ ExcelToWord/Program.cs
@@ -1,25 +1,13 @@
+using ExcelToWord.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.ConfigureDefaults("ExcelToWord");
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+await app.ConfigureDefaults();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+await app.RunAsync();
